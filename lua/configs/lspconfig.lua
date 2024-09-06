0376728f3bdb2c210b1976254b8e78ab8e959c6d
Teeco123 <kacper.gajko1@gmail.com>
2024-09-06 18:42:10 +0200

    Added rust LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "svelte", "lua_ls", "tsserver", "csharp_ls", "zls" }
+local servers = { "html", "cssls", "clangd", "svelte", "lua_ls", "tsserver", "csharp_ls", "zls", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
