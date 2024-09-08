1e09d480ba8bace1dd979eac86d52a675a87227a
Teeco123 <kacper.gajko1@gmail.com>
2024-09-08 04:13:00 +0200

    Added finder cycle center

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,5 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
-
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
