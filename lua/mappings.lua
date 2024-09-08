83234fb2944c173876f62e8f4aca30b75e60a206
Teeco123 <kacper.gajko1@gmail.com>
2024-09-08 04:01:51 +0200

    Added C-d C-u zz mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
