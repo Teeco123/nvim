2f08ceec06fa8bb4f4189e2ea888639823d1bf96
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-17 14:27:58 +0200

    Updated mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,14 +4,20 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("n", "<C-d>", "<C-d>zz", { desc = "Move half screen up and set cursor center" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Move half screen down and set cursor center" })
+-- Move harlf screen up/down and set cursor center
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+-- Iterate through search results
 map("n", "n", "nzz")
 map("n", "N", "Nzz")
-map("n", "<Up>", "<Nop")
-map("n", "<Left>", "<Nop")
-map("n", "<Right>", "<Nop")
-map("n", "<Down>", "<Nop")
+-- Disable arrows in normal Mode
+map("n", "<Up>", "<Nop>")
+map("n", "<Left>", "<Nop>")
+map("n", "<Right>", "<Nop>")
+map("n", "<Down>", "<Nop>")
+-- Disable arrows in insert Mode
+map("i", "<Up>", "<Nop>")
+map("i", "<Left>", "<Nop>")
+map("i", "<Right>", "<Nop>")
+map("i", "<Down>", "<Nop>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
